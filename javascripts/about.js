a438de0ac8b93c8e99c2459131ac17655aa9182e
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2018-04-04 16:02:00 -0500

    push about page

--- javascripts/about.js
+++ javascripts/about.js
@@ -6,6 +6,14 @@ let $ = require('jquery');
 function show(){
     $('#about').addClass("active");
     $(".main").addClass("gray");
+    $(".main").append(`
+    <div id="sexy" class="col-sm-12">
+        <img src="images/DrTeresaVasquez.png"/>
+        <h1>/ MY STORY</h1>
+        <p>I'm a product detail. I'm a great place to add more information about your product such as sizing, material, care and cleaning instructions. This is also a great space to write what makes this product special and how your customers can benefit from this item.</p>
+        <p class="last">I'm a product detail. I'm a great place to add more information about your product such as sizing, material, care and cleaning instructions. This is also a great space to write what makes this product special and how your customers can benefit from this item.</p>
+    </div>
+    `);
 }
 
 module.exports = {show};
--- javascripts/homepageComponents/carousel.js
+++ javascripts/homepageComponents/carousel.js
@@ -1,28 +1,51 @@
 "use strict";
 console.log("carousel linked");
 
-let $ = require('jquery');
+let $ = require('jquery'),
+shop = require('./../shop');
     
-
 function showCarousel(){
     let carImages = ["home_banner_blue3.png", "home_banner_lav2.png", "home_banner_pink1.png"];
 
-    let component = `
+    $(".main").append(`
     <div id="homePageCarousel" class="carousel slide">
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <img class="d-block w-100" src="./images/homeimages/${carImages[0]}" alt="First slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <h1>100% NATURAL.<br>
+                    HOMEMADE<br>
+                    FRESH COOKIES.</h1>
+                    <button type="button" class="btn btn-light">SHOP NOW</button>
+              </div>
             </div>
             <div class="carousel-item">
                 <img class="d-block w-100" src="./images/homeimages/${carImages[1]}" alt="Second slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <h1>100% NATURAL.<br>
+                    HOMEMADE<br>
+                    FRESH COOKIES.</h1>
+                    <button type="button" class="btn btn-light">SHOP NOW</button>
+              </div>
             </div>
             <div class="carousel-item">
                 <img class="d-block w-100" src="./images/homeimages/${carImages[2]}" alt="Third slide">
+                <div class="carousel-caption d-none d-md-block">
+                    <h1>100% NATURAL.<br>
+                    HOMEMADE<br>
+                    FRESH COOKIES.</h1>
+                    <button type="button" class="btn btn-light">SHOP NOW</button>
+              </div>
             </div>
         </div>
-    </div>`;
+    </div>`);
 
-    return component;
+    $(".btn").click((e)=>{
+        $(".main").html("");
+        $(".nav-link").removeClass("active");
+        $(".main").removeClass("gray");
+        shop.show();
+    });
 }
 
 module.exports = {showCarousel};
